a30e8238cb5bdf599ed62de5febb9d755541180e
assakaali <assakaaliouassim@outlook.fr>
2021-10-01 17:03:08 +0200

    ouassim page acceuil

--- Acceuil.php
+++ Acceuil.php
@@ -1,18 +1,51 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
+
+   <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style.css" media="all"/>
+    <title>CDA Projet | Acceuil</title>
+  </head>
 
-    <title>CDA Projet</title>
-</head>
-<body>
-    <h1>Bonjour</h1>
-    <form action="test.php">
-        <input></input>
-        <button type="submit"></button>
-    </form>
-</body>
 </html>
+
+<header class="main-head"> 
+     <nav> 
+         <h1 id="logo">BANK</h1>
+        
+        <ul>
+          <li>Accueil</li>
+          <li>menu</li>
+          <li>Contact</li>
+    </ul>
+
+    </nav>
+</header>
+
+    <main>
+
+      <section id="recherche client" ></section>
+    </main>
+
+</section>
+
+
+
+</section>
+
+    <section class="hero">
+    
+    <section id="recherche client" >
+       
+    <h1>Rechercher un client </h1>
+      
+    <form>
+        <input type="Identifiant" placeholder="Entrer Identifiant..." />
+           
+   </form>
+
+    <button type="submit" >Acceder</button>
+    
+    </section>
