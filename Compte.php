e9a68091f4bd03c0730cec81d5a3d13f4b0fd49a
Fazil Nabavi <eskandarnabavi@gmail.com>
2021-10-04 10:41:17 +0200

    fazil commit 1

--- Compte.php
+++ Compte.php
@@ -4,9 +4,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="stylecompte.css" media="all"/>
     <title>Compte</title>
-</head>
+</head>    
+<head class="main-head">
+<nav>
+
+<h1 id="logo"> | THE-BANK $ |</h1>
+
+</nav>
+</header>
+
 <body>
-    
+<main>
+    <h4>Type de Compte <b>Livret A </b></h4>
+    <hr/><hr> 
+    <p>Nom :</p>
+    <p>Prénom :</p>
+    <p>Date de naissance :</p>
+    <p>Email :</p>
+    <hr/><hr>
+    <p>Numero de compte :</p>
+    <p>Solde INT :</p>
+    <p>Découvert autorisé TINYINT :</p>
+</main>
 </body>
+
 </html>
