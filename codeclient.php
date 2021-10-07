24e4caa6770091d459fd4bec4f345c7ad546aba1
MauCar62 <mauny.carpentier@laposte.netgit config--global user.name MauCar62>
2021-10-07 16:53:23 +0200

    MauCar6

--- codeclient.php
+++ codeclient.php
@@ -2,6 +2,10 @@
 $codeclient=fopen('.\bd\Client.csv', 'r');
 
 $client=[];
+$nom=["CODE_AGENCE,ID_CLI,NOM_CLI,PRENOM_CLI,DATE_DE_NAISSANCE,EMAIL
+10,1,10,10,10,10
+1,1,Blaya,Mike,20/10/1995,blaya@gmail.com"];
+$identifiant=[];
 
 $code=readline("entrez votre identifiant : ");
 
@@ -10,12 +14,21 @@ foreach ($client as $value) {
     if($client[$i] && $value[0]==$code){
         print_r($value);
         break;
-        fgetcsv($codeclient);
+        fgetcsv($codeclient, ',');
     }  
+    elseif ($nom[$i] && $value[0]==$code){
+        print_r($value);
+        break;
+        fgetcsv($codeclient, ',');
+    }
+    elseif($identifiant[$i] && $value[0]==$code){
+        print_r($value);
+        break;
+        fgetcsv($codeclient, ',');
+    }
     else {
         echo("client introuvable");
          }  
     $i++;
-    
-}
+    }
 fclose($codeclient);
