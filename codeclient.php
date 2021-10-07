6959037222f39c210d144336aa544dc9647955e7
MauCar62 <mauny.carpentier@laposte.netgit config--global user.name MauCar62>
2021-10-07 13:55:59 +0200

    Maucommit4

--- codeclient.php
+++ codeclient.php
@@ -1,11 +1,7 @@
 <?php 
-$codeclient=fopen('Client.csv', 'w+');
+$codeclient=fopen('.\bd\Client.csv', 'r');
 
-$client=[
-    [001,"Client 01 : débiteur, Martin Jacques, 17/10/1970,Agence d'Arras"],
-    [002,"Client 02 : 12345 Type : créancier, Jean Dupont  15/06/1960, Agence de Béthune"],
-    [003,"Client 03 : 56789 Type : créanciere, Martine Delporte, 05/02/1980, Agence de Lille"]
-    ];
+$client=[];
 
 $code=readline("entrez votre identifiant : ");
 
@@ -14,7 +10,7 @@ foreach ($client as $value) {
     if($client[$i] && $value[0]==$code){
         print_r($value);
         break;
-        fputcsv($codeclient, $value);
+        fgetcsv($codeclient);
     }  
     else {
         echo("client introuvable");
