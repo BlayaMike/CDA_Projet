54244d39618b266625e653a8f6aa910fba867572
BlayaMike <blaya.mike62@gmail.com>
2021-10-15 09:54:09 +0200

    22ème commit BM

--- dto/Agence.php
+++ dto/Agence.php
@@ -1,14 +1,32 @@
 <?php
 class Agence {
 
-    public int $code_Agence;
-    public string $nom_Agence;
-    public string $adress_Agence;
+    private int $code_Agence;
+    private string $nom_Agence;
+    private string $adress_Agence;
     
     public function __construct(int $code_Agence, string $nom_Agence,string $adress_Agence){
-        $this->id       = $code_Agence;
+        $this->code       = $code_Agence;
         $this->nom      = $nom_Agence;
         $this->adress   = $adress_Agence;
+    }
+    public function getCode_Agence(){
+        return $this->code;
+    }        
+    public function setCode_Agence(int $code_Agence){
+        return $this->code = $code_Agence;
+    }
+    public function getNom_Agence(){
+        return $this->code;
+    }        
+    public function setNom_Agence(string $nom_Agence){
+        return $this->nom = $nom_Agence;
+    }
+    public function getAdress(){
+        return $this->adress;
+    }        
+    public function setAdress(string $adress){
+        return $this->adress = $adress;
     }
 }
 
